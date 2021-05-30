007fdd9c3d65e42101e214ac98292525cf735819
Enrique Walter Philippeaux <enriquewph@gmail.com>
2021-05-30 01:40:45 -0300

    adjusted ADC.

--- lib/hw_hal/include/temp.h
+++ lib/hw_hal/include/temp.h
@@ -5,14 +5,14 @@
 #include <hw_define.h>
 
 
-#define VCC_MV 3300.0
+#define VCC_MV 3300.0f
 #define ADC_RES 4096
-#define MV_PER_DEGREE 10.0
+#define MV_PER_DEGREE 10.0f
 #define CORRECCION_PORCENTUAL 1.0
-#define OFFSET 0.0
-#define PROM_READING 5
+#define OFFSET 151.18f
+#define PROM_READING 50
 
-// TODO: Ajustar por atenuacion.
+// Debido al funcionamiento del adc, las lecturas con correctas a partir de los 15ºC.
 
 /**
  * @brief Inicializa lo necesario para usar el sensor de temperatura.
@@ -39,12 +39,15 @@ float temp_reading_to_degrees(uint16_t reading);
  * @brief Macro para convertir lectura de ADC en grados centigrados (float).
  * 
  */
-#define LM35READMACRO(reading) (float) OFFSET + (CORRECCION_PORCENTUAL * (float) reading * VCC_MV) / (ADC_RES * MV_PER_DEGREE)
+#define LM35READMACRO(reading) (float) (CORRECCION_PORCENTUAL * ((float) reading + OFFSET) * VCC_MV) / (ADC_RES * MV_PER_DEGREE)
+
+//TODO: exportar a hal_adc
+#define ADC_GET_MV(reading) (float) (CORRECCION_PORCENTUAL * ((float) reading + OFFSET) * VCC_MV) / (ADC_RES)
 
 /**
  * @brief Macro para convertir grados centigrados (float) en aprox. la lectura del ADC requerida.
  * 
  */
-#define LM35READREVERSEMACRO(temp) (temp - OFFSET) * ((MV_PER_DEGREE * ADC_RES) / (CORRECCION_PORCENTUAL * VCC_MV))
+#define LM35READREVERSEMACRO(temp) (temp) * ((MV_PER_DEGREE * ADC_RES) / (CORRECCION_PORCENTUAL * VCC_MV)) - OFFSET
 
 #endif
