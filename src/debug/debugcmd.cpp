066226812c711f74c42a5dac092f202cd81d0b44
Enrique Walter Philippeaux <enriquewph@gmail.com>
2021-06-06 00:08:40 -0300

    fix debug typo

--- src/debug/debugcmd.cpp
+++ src/debug/debugcmd.cpp
@@ -11,8 +11,8 @@ cmd_t cmdList[] = {
     {"GETRELAY2", cmd_getRelay2, "Get relay 2 output state", strNone},
     {"SETFAN", cmd_setFan, "Set fan duty cycle", "<0-255> duty cycle %"},
     {"GETFAN", cmd_getFan, "Get fan duty cycle", strNone},
-    {"SETUPSOFF", cmd_setUPSON, "Set UPS_OFF state", "<0,1> output level"},
-    {"GETUPSOFF", cmd_getUPSON, "Get UPS_OFF state", strNone},
+    {"SETUPSOFF", cmd_setUPSON, "Set UPS_ON state", "<0,1> output level"},
+    {"GETUPSOFF", cmd_getUPSON, "Get UPS_ON state", strNone},
     {"GETUPSSTATE", cmd_getUPSSTATE, "Get all UPS led values", strNone}
 };
 uint16_t cmdList_len = sizeof(cmdList) / sizeof(cmdList[0]);
@@ -65,7 +65,7 @@ void cmd_getUPSON(Stream &StreamPort, char *param)
 
 void cmd_getUPSSTATE(Stream &StreamPort, char *param)
 {
-    StreamPort.printf("     UPS_OFF: %u\n", hal_ups_get_on());
+    StreamPort.printf("      UPS_ON: %u\n", hal_ups_get_on());
     StreamPort.printf("  UPS_NORMAL: %u\n", hal_ups_get_normal()); //TODO: agregar estas lecturas a hal. Deben estar invertidas.
     StreamPort.printf("   UPS_CARGA: %u\n", hal_ups_get_carga());
     StreamPort.printf("UPS_INVERTER: %u\n", hal_ups_get_falla()); //Este pin prende y apaga cuando funciona a BAT.
